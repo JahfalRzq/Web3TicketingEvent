a934372a2c7b14e3171af2a8bf58bb9c2394e4ed
jahfalrizqi <jahfalrizqi14@gmail.com>
2025-01-29 17:52:41 +0700

    first commit with user management

---
+++ src/model/User.ts
@@ -0,0 +1,83 @@
+import { IsString,IsUppercase } from "class-validator";
+import { Entity,PrimaryGeneratedColumn,Column,CreateDateColumn,UpdateDateColumn,DeleteDateColumn, OneToMany } from "typeorm";
+import bcrypt from 'bcryptjs';
+
+
+export enum UserRole {
+    ADMIN = 'ADMIN',
+    USER = 'USER',
+    EVENTORGANIZER = 'EVENTORGANIZER',
+
+}
+
+@Entity()
+export class User{
+
+    @PrimaryGeneratedColumn('uuid')
+    public id : string
+
+    
+    @Column({
+        default: null,
+        nullable: true
+    })
+    @IsString()
+    public namaLengkap: string
+
+
+    @Column({
+        default: null,
+        nullable: true
+    })
+    @IsString()
+    public userName: string
+
+    @Column({
+        default: null,
+        nullable: true
+    })
+    @IsString()
+    public password: string
+
+
+    @Column({
+        type: 'enum',
+        enum: UserRole,
+    })
+    @IsString()
+    @IsUppercase()
+    public role: UserRole
+
+    @Column({
+        default: null,
+        nullable: true
+    })
+    @IsString()
+    public eTTD: string
+
+    @Column({
+        default: null,
+        nullable: true
+    })
+    @IsString()
+    public noTelp: string
+    
+
+    @CreateDateColumn()
+    public createdAt: Date
+
+    @UpdateDateColumn()
+    public updatedAt: Date
+
+    @DeleteDateColumn()
+    public deletedAt: Date
+
+
+    public hashPassword() {
+        this.password = bcrypt.hashSync(this.password, 8)
+    }
+
+    public checkIfPasswordMatch(unencryptedPassword: string): boolean {
+        return bcrypt.compareSync(unencryptedPassword, this.password)
+    }
+}
