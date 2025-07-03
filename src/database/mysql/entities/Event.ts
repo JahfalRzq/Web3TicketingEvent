96e5d535339f1bc138f7cfd8802ccfb009fc6328
JahfalRzq <126737235+JahfalRzq@users.noreply.github.com>
2025-07-03 07:29:42 +0700

    feat : new entities Event for mysql

---
+++ src/database/mysql/entities/Event.ts
@@ -0,0 +1,121 @@
+import { IsDate, IsDecimal, IsInt, IsString,IsUppercase } from "class-validator";
+import { Entity,PrimaryGeneratedColumn,Column,CreateDateColumn,UpdateDateColumn,DeleteDateColumn, OneToMany, Collection,Int32, Decimal128, ManyToOne, JoinColumn } from "typeorm";
+import { User } from "./User";
+import { Ticket } from "./Ticket";
+
+
+export enum categoryEvent {
+    BLOCKCHAIN = 'BLOCKCHAIN',
+    KRIPTO = 'KRIPTO',
+    DEFI = 'DEFI',
+    NFT = 'NFT',
+    Web3 = 'Web3',
+    JobsHunter = 'JobsHunter',
+    AirdropHunter = 'AirdropHunter',
+}
+
+
+export enum typeEvent {
+    Nasional = 'Nasional',
+    Internasional = 'Internasional'
+}
+
+@Entity()
+export class Event{
+    @PrimaryGeneratedColumn('uuid')
+    public id : string
+
+    @Column({
+        default : null,
+        nullable : true
+    })
+    @IsString()
+    public nameEvent : string
+
+    @Column({
+    default : null,
+    nullable : true
+    })
+    @IsDate()
+    public startDate : Date
+
+    @Column({
+    default : null,
+    nullable : true
+    })
+    @IsDate()
+    public endDate : Date
+
+    @Column({
+    default : null,
+    nullable : true
+    })
+    @IsString()
+    public alamatEvent : String
+
+    @Column({
+        type: 'enum',
+        enum: categoryEvent,
+    })
+    @IsString()
+    @IsUppercase()
+    public categoryEvent: categoryEvent
+
+    @Column({
+        type: 'enum',
+        enum: typeEvent,
+    })
+    @IsString()
+    @IsUppercase()
+    public typeEvent: typeEvent
+
+
+    @Column({
+    default : null,
+    nullable : true
+    })
+    @IsInt()
+    public totalTicket : Int32
+
+    @Column({
+    default : null,
+    nullable : true
+    })
+    @IsDecimal()
+    public ticketPrice : Decimal128
+
+    
+    @Column({
+        nullable : false
+    })
+    @IsString()
+    public contract_address : string
+
+        
+    @Column({
+        nullable : false
+    })
+    @IsString()
+    public metadata_uri : string
+
+
+
+    @CreateDateColumn()
+    public createdAt: Date
+
+    @UpdateDateColumn()
+    public updatedAt: Date
+
+    @DeleteDateColumn()
+    public deletedAt: Date
+
+    @ManyToOne (() => User, (userOrganizer) => userOrganizer.events)
+    @JoinColumn()
+    public userOrganizer : User
+
+    @OneToMany(() => Ticket, (tickets) => tickets.event_id)
+    public tickets : Ticket[]
+
+    
+    
+    }
