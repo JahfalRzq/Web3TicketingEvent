ed0c55f14b34371ddc1d529d8a3ba35fe584b145
JahfalRzq <126737235+JahfalRzq@users.noreply.github.com>
2025-07-09 10:35:30 +0700

    refactor : fix create jwt payload

--- src/utils/createJwtToken.ts
+++ src/utils/createJwtToken.ts
@@ -1,9 +1,15 @@
-import jwt from 'jsonwebtoken'
+import jwt, { SignOptions } from 'jsonwebtoken'
 import { JwtPayload } from '../types/JwtPayload'
-require('dotenv').config()
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 export const createJwtToken = (payload: JwtPayload): string => {
-  return jwt.sign(payload, process.env.JWT_SECRET!, {
-    expiresIn: process.env.JWT_EXPIRATION,
-  })
+  const options: SignOptions = {
+    expiresIn: process.env.JWT_EXPIRATION ? 
+      parseInt(process.env.JWT_EXPIRATION) : 
+      2592000 // default 1 jam dalam detik
+  }
+
+  return jwt.sign(payload, process.env.JWT_SECRET!, options)
 }
