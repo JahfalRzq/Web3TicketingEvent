b4df5880f88ebc47f9ff4d16cd126104e1d06ed5
JahfalRzq <126737235+JahfalRzq@users.noreply.github.com>
2025-07-28 09:37:39 +0700

    refactor : fix failed unit test using hardhat and ethers

--- test/EventNFT.test.js
+++ test/EventNFT.test.js
@@ -1,26 +1,56 @@
 const { expect } = require("chai");
+const { ethers } = require("hardhat");
 
-describe("EventNFT", function () {
-    let EventNFT, eventNFT, owner, addr1;
+describe("EventNFT Contract", function () {
+  let EventNFT, eventNFT, owner, addr1;
 
-    beforeEach(async function () {
-        EventNFT = await ethers.getContractFactory("EventNFT");
-        [owner, addr1] = await ethers.getSigners();
-        eventNFT = await EventNFT.deploy();
-        await eventNFT.deployed();
-    });
+  beforeEach(async () => {
+    [owner, addr1] = await ethers.getSigners();
+    EventNFT = await ethers.getContractFactory("EventNFT");
+    eventNFT = await EventNFT.deploy();
+    await eventNFT.waitForDeployment();
+  });
 
-    it("Should mint an NFT", async function () {
-        const tokenURI = "https://example.com/metadata/1";
-        await eventNFT.mintNFT(addr1.address, tokenURI);
-        const ownerOfToken = await eventNFT.ownerOf(1);
-        expect(ownerOfToken).to.equal(addr1.address);
-    });
+  it("Should deploy with correct name and symbol", async () => {
+    expect(await eventNFT.name()).to.equal("EventNFT");
+    expect(await eventNFT.symbol()).to.equal("EVNT");
+  });
 
-    it("Should return the correct token URI", async function () {
-        const tokenURI = "https://example.com/metadata/1";
-        await eventNFT.mintNFT(addr1.address, tokenURI);
-        const uri = await eventNFT.tokenURI(1);
-        expect(uri).to.equal(tokenURI);
-    });
+  it("Should mint an NFT and assign it to addr1", async () => {
+    const tokenURI = "https://example.com/token/1.json";
+
+    const tx = await eventNFT.connect(owner).mintNFT(addr1.address, tokenURI);
+    await tx.wait();
+
+    const tokenId = 0;
+    expect(await eventNFT.ownerOf(tokenId)).to.equal(addr1.address);
+    expect(await eventNFT.tokenURI(tokenId)).to.equal(tokenURI);
+  });
+
+  it("Should emit NFTMinted event on minting", async () => {
+    const tokenURI = "https://example.com/token/1.json";
+
+    const tx = await eventNFT.connect(owner).mintNFT(addr1.address, tokenURI);
+    const receipt = await tx.wait();
+
+    const event = receipt.logs
+      .map(log => eventNFT.interface.parseLog(log))
+      .find(log => log.name === "NFTMinted");
+
+    expect(event).to.not.be.undefined;
+    expect(event.args[0]).to.equal(0n);
+    expect(event.args[1]).to.equal(addr1.address);
+    expect(event.args[2]).to.equal(tokenURI);
+  });
+
+  it("Should revert if non-owner tries to mint", async () => {
+    let reverted = false;
+    try {
+      await eventNFT.connect(addr1).mintNFT(addr1.address, "uri");
+    } catch (error) {
+      reverted = true;
+      expect(error.message).to.include("Ownable: caller is not the owner");
+    }
+    expect(reverted).to.be.true;
+  });
 });
--- test/EventTicket.test.js
+++ test/EventTicket.test.js
@@ -1,28 +1,56 @@
 const { expect } = require("chai");
+const { ethers } = require("hardhat");
 
-describe("EventTicket", function () {
-  let EventTicket, eventTicket, owner, addr1;
+describe("EventNFT Contract", function () {
+  let EventNFT, eventNFT, owner, addr1;
 
-  beforeEach(async function () {
-    EventTicket = await ethers.getContractFactory("EventTicket");
+  beforeEach(async () => {
     [owner, addr1] = await ethers.getSigners();
-    eventTicket = await EventTicket.deploy();
-    await eventTicket.deployed();
+    EventNFT = await ethers.getContractFactory("EventNFT");
+    eventNFT = await EventNFT.deploy();
+    await eventNFT.waitForDeployment();
   });
 
-  it("Should create a ticket", async function () {
-    await eventTicket.createTicket("Event 1", ethers.utils.parseEther("0.1"), 10);
-    const ticket = await eventTicket.tickets(1);
-    expect(ticket.eventName).to.equal("Event 1");
-    expect(ticket.price).to.equal(ethers.utils.parseEther("0.1"));
-    expect(ticket.totalSupply).to.equal(10);
-    expect(ticket.sold).to.equal(0);
+  it("Should deploy with correct name and symbol", async () => {
+    expect(await eventNFT.name()).to.equal("EventNFT");
+    expect(await eventNFT.symbol()).to.equal("EVNT");
   });
 
-  it("Should purchase a ticket", async function () {
-    await eventTicket.createTicket("Event 1", ethers.utils.parseEther("0.1"), 10);
-    await eventTicket.connect(addr1).purchaseTicket(1, 1, { value: ethers.utils.parseEther("0.1") });
-    const ticket = await eventTicket.tickets(1);
-    expect(ticket.sold).to.equal(1);
+  it("Should mint an NFT and assign it to addr1", async () => {
+    const tokenURI = "https://example.com/token/1.json";
+
+    const tx = await eventNFT.connect(owner).mintNFT(addr1.address, tokenURI);
+    await tx.wait();
+
+    const tokenId = 0;
+    expect(await eventNFT.ownerOf(tokenId)).to.equal(addr1.address);
+    expect(await eventNFT.tokenURI(tokenId)).to.equal(tokenURI);
+  });
+
+  it("Should emit NFTMinted event on minting", async () => {
+    const tokenURI = "https://example.com/token/1.json";
+
+    const tx = await eventNFT.connect(owner).mintNFT(addr1.address, tokenURI);
+    const receipt = await tx.wait();
+
+    const event = receipt.logs
+      .map(log => eventNFT.interface.parseLog(log))
+      .find(log => log.name === "NFTMinted");
+
+    expect(event).to.not.be.undefined;
+    expect(event.args[0]).to.equal(0n);
+    expect(event.args[1]).to.equal(addr1.address);
+    expect(event.args[2]).to.equal(tokenURI);
+  });
+
+  it("Should revert if non-owner tries to mint", async () => {
+    let reverted = false;
+    try {
+      await eventNFT.connect(addr1).mintNFT(addr1.address, "uri");
+    } catch (error) {
+      reverted = true;
+      expect(error.message).to.include("Ownable: caller is not the owner");
+    }
+    expect(reverted).to.be.true;
   });
 });
