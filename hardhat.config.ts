a40edd633b76a6202817ecdde5a322b0de2ecf5b
JahfalRzq <126737235+JahfalRzq@users.noreply.github.com>
2025-07-29 18:16:42 +0700

    refactor : comment hardhat chai matchers

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,5 +1,7 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@openzeppelin/hardhat-upgrades";
+// import "@nomicfoundation/hardhat-chai-matchers";
+
 
 const config: HardhatUserConfig = {
   solidity: "0.8.20",
