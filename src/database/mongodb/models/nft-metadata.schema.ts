742b83a483de3a69e920d62153c17217e38e3578
JahfalRzq <126737235+JahfalRzq@users.noreply.github.com>
2025-08-01 22:25:30 +0700

    feat : new schema for metadata nft on mongo db

---
+++ src/database/mongodb/models/nft-metadata.schema.ts
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+
+const NFTMetadataSchema = new mongoose.Schema(
+  {
+    eventId: {
+      type: Number,
+      required: true, // merujuk ke event_id di MySQL
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    description: String,
+    image: String, // bisa URL ke IPFS atau CDN
+    attributes: [
+      {
+        trait_type: String,
+        value: mongoose.Schema.Types.Mixed,
+      },
+    ],
+    contractAddress: {
+      type: String,
+      required: true,
+    },
+    metadataURI: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const NFTMetadataModel = mongoose.model("NFTMetadata", NFTMetadataSchema);
