5e76188b50e4f9491b14dc14835fbfd5bec99a67
JahfalRzq <126737235+JahfalRzq@users.noreply.github.com>
2025-08-03 16:53:04 +0700

    refactor : add checking jwt on create event stub route

--- src/routes/v1/event.routes.ts
+++ src/routes/v1/event.routes.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import { createEventStub } from "../../controller/Event/event.stub.controller";
+import { checkJwt } from "../../utils/checkJwt";
 
 const router = Router();
 
-router.post("/createEventStub", createEventStub);
+router.post("/createEventStub", [checkJwt,createEventStub]);
 
 export default router;
